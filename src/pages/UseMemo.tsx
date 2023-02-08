f6e15f54ffe685f4d376b8512f9efc237411e018
MBANK\gal.n <gal.n@m-bank.mn>
2023-02-08 17:30:12 +0800

    husky lint-staged test

--- src/pages/UseMemo.tsx
+++ src/pages/UseMemo.tsx
@@ -21,7 +21,7 @@ export default function UseReducer() {
         checked={isDark}
         onChange={() => setIsDark((prev) => !prev)}
       />
-      <h3>test ddsssd</h3>
+      <h3>test husky again</h3>
       <div
         className={`w-[200px] h-[50px] ${isDark ? 'bg-black' : 'bg-white'}`}
       ></div>
