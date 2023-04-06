1959869db0dcbc99940efc005f72bf1a1ee69bb1
glaadaa <glaadaa@gmail.com>
2023-04-06 11:24:47 +0800

    test

--- src/layout/SideBar.tsx
+++ src/layout/SideBar.tsx
@@ -6,7 +6,7 @@ function SideBar() {
     <div className="fixed left-0 top-0 h-[100vh] overflow-y-auto w-[240px] border border-r border-zinc-200 px-5 py-5 bg-white">
       <h3 className="text-lg text-black font-bold uppercase">All categories</h3>
       {error ? <div>{error.message}</div> : null}
-      {isLoading ? <span>Loading</span> : null}
+      {isLoading ? <span>Loading...</span> : null}
       {data ? (
         <ul className="mt-10">
           {data.map((category: string, index: number) => {
